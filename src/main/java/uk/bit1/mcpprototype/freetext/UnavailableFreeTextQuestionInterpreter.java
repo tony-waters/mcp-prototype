3c8354c034eb79e99b07535c99bda50817e15712
Tony Waters <2tonywaters@gmail.com>
2026-09-08 23:57:48 +0100

    fix free text feature error

--- src/main/java/uk/bit1/mcpprototype/freetext/UnavailableFreeTextQuestionInterpreter.java
+++ src/main/java/uk/bit1/mcpprototype/freetext/UnavailableFreeTextQuestionInterpreter.java
@@ -1,12 +1,13 @@
 package uk.bit1.mcpprototype.freetext;
 
+import org.springframework.ai.chat.model.ChatModel;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
-@ConditionalOnMissingBean(FreeTextQuestionInterpreter.class)
+@ConditionalOnMissingBean(ChatModel.class)
 class UnavailableFreeTextQuestionInterpreter implements FreeTextQuestionInterpreter {
 
     @Override
--- src/test/java/uk/bit1/mcpprototype/McpPrototypeApplicationTests.java
+++ src/test/java/uk/bit1/mcpprototype/McpPrototypeApplicationTests.java
@@ -1,13 +1,21 @@
 package uk.bit1.mcpprototype;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import uk.bit1.mcpprototype.freetext.FreeTextQuestionService;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class McpPrototypeApplicationTests extends PostgresIntegrationTest {
 
+    @Autowired
+    private FreeTextQuestionService freeTextQuestionService;
+
     @Test
-    void contextLoads() {
+    void startsWithFreeTextUnavailableWhenNoChatModelIsConfigured() {
+        assertThat(freeTextQuestionService.isAvailable()).isFalse();
     }
 
 }
